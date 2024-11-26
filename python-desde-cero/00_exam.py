f302e834b022dde96835d935b2116d6fb3710bd2
jfabriciovr <46113391+jfabriciovr@users.noreply.github.com>
2024-11-25 21:38:22 -0600

    Add Readme

---
+++ python-desde-cero/00_exam.py
@@ -0,0 +1,53 @@
+"""
+¿Qué tipo de dato imprime el siguiente código?
+print(type(3.14))
+
+"""
+# float
+# El valor 3.14 es un número con punto decimal, y en Python, los números con punto decimal se clasifican como tipo float
+
+"""
+¿Cómo se añade un comentario de una sola línea en Python?
+
+"""
+# Con el símbolo #
+
+"""
+¿Qué se imprime al ejecutar el código print("Hola, Python") ?
+"""
+# Hola, Python
+
+"""
+¿Qué tipo de dato se muestra al ejecutar type(5)?
+"""
+# int, ya que el número 5 es un entero
+
+"""
+¿Qué tipo de dato es True en Python?
+"""
+# bool, ya que True es un valor lógico o booleano
+
+"""
+¿Qué hace la operación type() en Python?
+"""
+# La función type() devuelve el tipo de dato de un objeto en Python
+
+"""
+¿Cómo se escribe un comentario en varias líneas en Python?
+"""
+# Con tres comillas dobles ("""...""") o tres comillas simples ('''...''')
+
+"""
+¿Qué resultado se obtiene al ejecutar print("Hola"+""+"Python")?
+"""
+# HolaPython, ya que se concatenan las cadenas de texto
+
+"""
+¿Cuál es la operación que se utiliza para mostrar un mensaje por pantalla en Python?
+"""
+# print()
+
+"""
+¿Cuál de las siguientes es una manera correcta de imprimir un número entero en Python?
+"""
+# print(5)
---
+++ python-desde-cero/README.md
@@ -0,0 +1,7 @@
+Enlaces de interés:
+
+- Página oficial de python:
+- Documentación Oficial de python: https://docs.python.org/es/3/
+- Tutorial oficial de python: https://docs.python.org/es/3/tutorial/index.html
+- Repositorio "30 días de Python": https://github.com/Asabeneh/30-Days-Of-Python
+- Juego Códex para aprender python: https://www.codedex.io/python
