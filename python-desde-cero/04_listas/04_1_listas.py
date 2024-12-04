aa7ff35dfb4ac316219446ebf92bcfa71afb3e5a
jfabriciovr <46113391+jfabriciovr@users.noreply.github.com>
2024-12-04 00:05:24 -0600

    Add listas

---
+++ python-desde-cero/04_listas/04_1_listas.py
@@ -0,0 +1,26 @@
+### LISTAS ###
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list)) # 0
+print(len(my_other_list)) # 0
+
+
+my_list = [35, 24, 62, 52, 30, 30, 17]
+
+print(my_list) # [35, 24, 62, 52, 30, 30, 17]
+print(len(my_list)) # 7
+
+my_other_list = [35, 1.77, "Brais", "Moure"]
+print(my_other_list) # [35, 1.77, 'Brais', 'Moure"]
+print(type(my_other_list)) # <class 'list'>
+print(my_other_list[0]) # 35
+print(my_other_list[1]) # 1.77
+print(my_other_list[-1]) # "Moure" es el último elemento
+print(my_other_list[-4]) # 35 es el primer elemento
+print(my_other_list.count("Brais")) 
+#print(my_other_list[-5]) IndexError
+#print(my_other_list[4]) IndexError
+
+
