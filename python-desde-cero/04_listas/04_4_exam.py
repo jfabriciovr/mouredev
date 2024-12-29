87d33ac5f305f9da9e7811ea284ff50311bc7342
jfabriciovr <46113391+jfabriciovr@users.noreply.github.com>
2024-12-28 18:38:08 -0600

    Create 04_4_exam.py

---
+++ python-desde-cero/04_listas/04_4_exam.py
@@ -0,0 +1,45 @@
+# ¿Cuál es el resultado de
+my_list = [1, 2, 3]
+my_list.clear()
+print(my_list)
+# []
+
+# ¿Cuál es el resultado de
+my_list = [1, 2, 3]
+my_list.append(4)
+print(my_list)
+# [1, 2, 3, 4]
+
+# ¿Qué operación cuenta cuántas veces aparece un valor en una lista?
+# count()
+
+# ¿Qué imprime este código?
+
+my_list = [3, 1, 2]
+my_list.sort()
+print(my_list)
+# [1, 2, 3]
+
+# ¿Qué método se utiliza para eliminar el último elemento de una lista?
+# pop() y además devuelve el número
+
+# ¿Qué operación se utiliza para agregar un elemento al final de una lista de Python?
+# append()
+
+#¿Qué imprime este código?
+my_list = [1, 2, 3, 4, 5]
+print(my_list[1:4])
+# [2, 3, 4]
+
+# ¿Cuál es el resultado de ejecutar el siguiente código?
+my_list = [1, 2, 3]
+my_list.insert(1, "a")
+print(my_list)
+# [1, 'a', 2, 3]
+
+# ¿Qué operación crea una copia superficial de una lista?
+# copy()
+
+# ¿Cuál es el propósito de la operación sort() en una lista?
+# ordenar la lista
+
