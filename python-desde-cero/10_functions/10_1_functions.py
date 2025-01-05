78b973d6a3d51eb08e39cc953853a850584319c9
jfabriciovr <46113391+jfabriciovr@users.noreply.github.com>
2025-01-04 23:05:46 -0600

    Update 10_1_functions.py

--- python-desde-cero/10_functions/10_1_functions.py
+++ python-desde-cero/10_functions/10_1_functions.py
@@ -36,33 +36,44 @@ print(my_result) # None - porque la función no retorna nada
 my_result = sum_two_values_with_return(10, 5)
 print(my_result) # 15
 
+
 # FunciÃ³n con parÃ¡metros de entrada/argumentos por clave
-
-
 def print_name(name, surname):
     print(f"{name} {surname}")
 
-
-print_name(surname="Moure", name="Brais")
+print_name(surname="Moure", name="Brais") # Brais Moure
 
 # FunciÃ³n con parÃ¡metros de entrada/argumentos por defecto
-
-
 def print_name_with_default(name, surname, alias="Sin alias"):
     print(f"{name} {surname} {alias}")
 
-
-print_name_with_default("Brais", "Moure")
-print_name_with_default("Brais", "Moure", "MoureDev")
+print_name_with_default("Brais", "Moure") # Brais Moure Sin alias
+print_name_with_default("Brais", "Moure", "MoureDev") # Brais Moure MoureDev
 
 # FunciÃ³n con parÃ¡metros de entrada/argumentos arbitrarios
+def print_texts(*text): # *text quiere decir argumentos infinitos
+    print(type (text))
+    print(text)
+
+print_texts("estos", "textos", "se imprimen como tuplas") 
+# <class 'tuple'>
+# ('estos', 'textos', 'se imprimen como tuplas')
 
 
 def print_upper_texts(*texts):
     print(type(texts))
-    for text in texts:
+    for text in texts: # recorre la tupla de elementos y va imprimiendo cada uno en una línea independiente
         print(text.upper())
 
 
 print_upper_texts("Hola", "Python", "MoureDev")
+# <class 'tuple'>
+# HOLA
+# PYTHON
+# MOUREDEV
+
 print_upper_texts("Hola")
+# <class 'tuple'
+# HOLA
+
+
