6596733f18481c0d78e3bca566f5c29e64458052
jfabriciovr <46113391+jfabriciovr@users.noreply.github.com>
2025-01-13 00:21:32 -0600

    Create 05_error_types.py

---
+++ python-desde-cero/Intermedio/05_error_types.py
@@ -0,0 +1,18 @@
+#######################
+### Error Types ###
+#######################
+
+# print "hola comunidad"
+# SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+
+# print(language)
+# NameError: name 'language' is not defined
+
+# my_list = [1,2]
+# print(my_list[4])
+# IndexError: list index out of range
+
+import maths
+
+
+
