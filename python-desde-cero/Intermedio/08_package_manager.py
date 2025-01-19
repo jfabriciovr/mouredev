1fd45fcfc285bf583b84e0d99fb4f3d424c3e7d1
jfabriciovr <46113391+jfabriciovr@users.noreply.github.com>
2025-01-18 23:47:21 -0600

    Create 08_package_manager.py

---
+++ python-desde-cero/Intermedio/08_package_manager.py
@@ -0,0 +1,34 @@
+### Python Package Manager ###
+
+# se utlizar una herramienta que se llama PIP
+# PIP https://pypi.org
+
+# para instalar pip con la terminal:
+# pip install pip
+
+#para ve qué version tenemos
+# pip --version
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# en la terminal podemos escribir: pip list
+# y nos va a dar una lista de todos los paquetes instalados
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@2
+# NUMPY
+# en la terminal usar: 
+    # pip install numpy
+    # pip unistall numpy
+    # pip show numpy
+import numpy
+
+numpy_array = numpy.array([35, 24, 62, 52, 30, 30, 17])
+print(type(numpy_array)) # <class 'numpy.ndarray'>
+print(numpy_array * 2) # [ 70  48 124 104  60  60  34]
+
+#@@@@@@@@@@@@@@@@@@@@@
+# PANDAS
+
+
+
+
+
